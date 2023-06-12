4a50ad36c9b19c2b4c1ece24527e5c51c9c522a9
Elkin Javier Guerra Galeano <elkinmt19@gmail.com>
2023-06-12 14:25:04 -0500

    Deleted some blanck spaces

--- projects/ecs-kinesis-timestream/data-producer/terraform/data.tf
+++ projects/ecs-kinesis-timestream/data-producer/terraform/data.tf
@@ -3,8 +3,8 @@ data "aws_iam_policy_document" "assume_role_policy" {
     statement {
         actions = ["sts:AssumeRole"]
         principals {
-        type        = "Service"
-        identifiers = ["ecs-tasks.amazonaws.com"]
+            type = "Service"
+            identifiers = ["ecs-tasks.amazonaws.com"]
         }
     }
 }
