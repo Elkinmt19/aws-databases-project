a364d8ec92ea106cd18b4f24ceba7954c75e0272
Elkin Javier Guerra Galeano <elkinmt19@gmail.com>
2023-06-12 14:13:24 -0500

    Deleted some blanck spaces

--- projects/ecs-kinesis-timestream/timestream/terraform/main.tf
+++ projects/ecs-kinesis-timestream/timestream/terraform/main.tf
@@ -8,10 +8,10 @@ resource "aws_timestreamwrite_database" "timestream_db" {
 }
 resource "aws_timestreamwrite_table" "timestream_table" {
     database_name = aws_timestreamwrite_database.timestream_db.database_name
-    table_name    =  var.timestream_table_name
+    table_name = var.timestream_table_name
     retention_properties {
         magnetic_store_retention_period_in_days = 30
-        memory_store_retention_period_in_hours  = 8
+        memory_store_retention_period_in_hours = 8
     }
     tags = {
         name = "${var.project_name}-timestream-db"
